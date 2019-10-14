8da4a2373529f083bfed5ca798cd4964886a2db1
SHOBHIT <shobhitsaini95@gmail.com>
2019-10-14 21:12:04 +0530

    Update code.cpp

--- Reverse_words_in_a_given_string/code.cpp
+++ Reverse_words_in_a_given_string/code.cpp
@@ -9,23 +9,25 @@ int main()
 	string str1, temp ="";
 	char c;
 	cout << "Enter the string:";
-	cin >> str1;
+	getline(cin,str1);
 	string str2[strlen(str1.c_str())];
 	int i = 0, j = 0;
+	
 	for(i = 0; i < strlen(str1.c_str()); i++)
 	{
-		while(str1[i] != ' ')
+		while(str1[i] != ' ' && i < strlen(str1.c_str()))
 		{
-			temp.append(&str1[i]);
+			temp+=(str1[i]);
+			i++;
+			
 		}
-		i++;
+		temp+=' ';
 		str2[j++] = temp;
 		temp = "";
 	}
 	
-	for(i = j-1; i >= 0; i--)
+	for(i = j - 1; i >= 0; i--)
 	{
-		cout << str2[i];
+	    cout<<str2[i];
 	}
-	
 }
